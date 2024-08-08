aae18d8e91f3206df3c435527fbfbe7d8dbf8c69
Sylvia <sylvia.waweru@gmail.com>
2024-08-08 16:57:05 +0300

    code changes

--- Backend/lambda-function.py
+++ Backend/lambda-function.py
@@ -4,7 +4,7 @@ import boto3
 
 def lambda_handler(event, context):
     client=boto3.resource('dynamodb')
-    table=client.Table('restart-table')
+    table=client.Table('resume-table')
     try:
         response=table.scan()
         items=response['Items']
